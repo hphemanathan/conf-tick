10c3bef38eb8832e7beb39012e3dcb5efca221a0
Hemanathan H P <hphemanathan@gmail.com>
2025-04-27 20:44:24 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='mt-8 mx-auto flex items-center justify-center'>
+      <img src='src/assets/logo-full.svg' alt='' />
+    </div>
+  );
+}
+
+export default Header;
